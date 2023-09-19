1002d14923342610fca12795641fbc832d38f555
anateston <anapaulateston@gmail.com>
2023-09-19 10:45:55 +0200

    built first search bar & filtering launches in 3 categories: success, failure and future

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,23 @@
-const SearchBox = () => {
+import React from 'react';
+
+const SearchBox = (props) => {
+  const { placeholder, onChangeHandler } = props;
+
+  const handleChange = (event) => {
+    // access to event.target.value
+    const inputValue = event.target.value;
+    // Call the provided onChangeHandler function
+    onChangeHandler(inputValue);
+  }
+
   return (
-    <div>search</div>
+    <input
+      className="searchBox"
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
   )
 }
-export default SearchBox
+
+export default SearchBox;
